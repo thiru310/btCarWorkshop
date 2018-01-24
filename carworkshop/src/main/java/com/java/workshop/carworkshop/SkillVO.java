1b6e391d85825d3543ee1a5b98efd0c5133ae9e1
Thirumurugan P <Thirumurugan P@Thirumurugan.in.ibm.com>
2018-01-24 23:42:49 +0530

    New Pojo Class

---
+++ carworkshop/src/main/java/com/java/workshop/carworkshop/SkillVO.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.java.workshop.carworkshop;
+
+/**
+ * @author Thirumurugan P
+ *
+ */
+public class SkillVO {
+	
+	private Integer skillId;
+	private String skillName;
+
+}
